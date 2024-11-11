2b468674a7b131f9d85a4b25797c5258c2652f54
lucifer <sako.delovoi@gmail.com>
2024-11-11 17:29:28 +0600

    SakoFilms v1

---
+++ index.php
@@ -0,0 +1,83 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    />
+    <link
+      rel="stylesheet"
+      href="./css/style.css"
+    />
+    <link
+      rel="stylesheet"
+      href="./css/fonts.css"
+    />
+    <title>Sako Films</title>
+  </head>
+  <body
+    style="width: 100vw"
+    class="centered"
+  >
+    <header class="centered">
+      <div>
+        <ol class="links">
+          <a
+            href="#"
+            style="text-decoration: none"
+            ><li class="link">Мое кино</li></a
+          >
+          <a
+            href="#"
+            style="text-decoration: none"
+            ><li class="link">Магазин</li></a
+          >
+          <a
+            href="#"
+            style="text-decoration: none"
+            ><li class="link">Мои покупки</li></a
+          >
+        </ol>
+      </div>
+    </header>
+    <section class="centered">
+      <img
+        src="./img/films-preview.svg"
+        alt=""
+        class="hero-preview"
+      />
+      <h1 class="hero-text">
+        SakoFilms <span>4К</span><br />Лучшие фильмы и сериалы
+      </h1>
+    </section>
+
+    <section class="new-films">
+      <h1 style="font-size: 50px">НОВИНКИ</h1>
+      <div class="films centered">
+        <div class="container">
+          <div>
+            <img
+              src="https://avatars.mds.yandex.net/get-kinopoisk-image/1898899/0751c132-28dc-4423-9a1a-3e4b61b6ebbe/1920x"
+              alt="Film Image"
+              class="film-image"
+              width="350px"
+            />
+          </div>
+          <div class="film-desc">
+            <h1 class="film-name">Гарфилд</h1>
+            <a
+              style="color: var(--contrast)"
+              target="_blank"
+              href="https://www.kinopoisk.ru/film/1044220/"
+              >Посмотреть</a
+            >
+            <span class="rating">6.3</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
